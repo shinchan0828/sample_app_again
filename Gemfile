a69eaacde507ab04c218cc4845f5836adf99950d
Marcos <shinji.yamamoto0828@gmail.com>
2019-01-26 19:45:32 +0900

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap-sass'
 gem 'bcrypt'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -18,12 +22,6 @@ module SessionsHelper
         log_in user
         @current_user = user
       end
-    end
-  end
-
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
@@ -41,5 +39,14 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
